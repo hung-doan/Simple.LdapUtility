4c8452ffff30806ebb6a530ab867178b1892376b
Hung Doan <hung.doantan@gmail.com>
2015-08-15 13:23:15 +0700

    init

---
+++ Simple.LdapUtility/LdapUtility.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.LdapUtility
+{
+    public class LdapUtility
+    {
+        private readonly string _adPath;
+        private readonly string _userName;
+        private readonly string _password;
+        /// <summary>
+        /// Authorize the user input by UserName and password with default AuthenticationTypes:
+        /// AuthenticationTypes = Secure
+        /// </summary>
+        /// <param name="adPath">LDAP://HostName[:PortNumber][/DistinguishedName]</param>
+        /// <param name="userName">User name</param>
+        /// <param name="password">User password in plain text</param>
+        /// <returns>true if user is authorized</returns>
+        /// <example>
+        /// var ldapUtility = new LdapUtility("LDAP://server01:390", "hungdoan", "pwd");
+        /// </example>
+        public LdapUtility(string adPath, string userName, string password)
+        {
+            _adPath = adPath;
+            _userName = userName;
+            _password = password;
+        }
+        
+        public bool Authenticate()
+        {
+            DirectoryEntry entry = new DirectoryEntry(
+                _adPath,
+                _userName,
+                _password,
+                AuthenticationTypes.Secure);
+            try
+            {
+                //Bind to the native AdsObject to force authentication.			
+                var obj = entry.NativeObject;
+
+            }
+            catch (COMException ex)
+            {
+                return false;
+            }
+            return true;
+        }
+        public bool Authenticate(out COMException outPutException)
+        {
+            DirectoryEntry entry = new DirectoryEntry(
+                 _adPath,
+                 _userName,
+                 _password,
+                 AuthenticationTypes.Secure);
+            try
+            {
+                //Bind to the native AdsObject to force authentication.			
+                var obj = entry.NativeObject;
+            }
+            catch (COMException ex)
+            {
+                outPutException = ex;
+                return false;
+            }
+
+            outPutException = null;
+            return true;
+        }
+
+        
+    }
+}
---
+++ UnitTest.Simple.LdapUtiity/LdapUtilityTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simple.LdapUtility;
+
+namespace UnitTest.Simple.LdapUtiity
+{
+    [TestClass]
+    public class LdapUtilityTest
+    {
+        public string validPathByIP = "LDAP://192.168.1.9";
+        public string validPathByFullyDNS = "LDAP://rims.local";
+
+        public string validUserName = "test";
+        public string validPassword = "Abc123456";
+
+        public string inValidPath = "LDAP://192.168.1._";
+        public string inValidUserName = "test_";
+        public string inValidPassword = "Abc123456_";
+        [TestMethod]
+        public void Authenticate_Authorized_Use_IP()
+        {
+            var utility = new LdapUtility(validPathByIP, validUserName, validPassword);
+            var isAuthenticated = utility.Authenticate();
+            Assert.IsTrue(isAuthenticated);
+        }
+        [TestMethod]
+        public void Authenticate_Authorized_Use_FullyDNS()
+        {
+            var utility = new LdapUtility(validPathByFullyDNS, validUserName, validPassword);
+            var isAuthenticated = utility.Authenticate();
+            Assert.IsTrue(isAuthenticated);
+        }
+        
+        [TestMethod]
+        public void Authenticate_Unauthorized_BecauseOf_Wrong_Path()
+        {
+            var utility = new LdapUtility(inValidPath, validUserName, validPassword);
+            var isAuthenticated = utility.Authenticate();
+            Assert.IsFalse(isAuthenticated);
+        }
+        [TestMethod]
+        public void Authenticate_Unauthorized_BecauseOf_Wrong_UserName()
+        {
+            var utility = new LdapUtility(validPathByIP, inValidUserName, validPassword);
+            var isAuthenticated = utility.Authenticate();
+            Assert.IsFalse(isAuthenticated);
+        }
+        [TestMethod]
+        public void Authenticate_Unauthorized_BecauseOf_Wrong_Password()
+        {
+            var utility = new LdapUtility(validPathByIP, validUserName, inValidPassword);
+            var isAuthenticated = utility.Authenticate();
+            Assert.IsFalse(isAuthenticated);
+        }
+    }
+}
